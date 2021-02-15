d4291dda5b5bfeee25a622947f4a4ed1fa64e3c3
b6320503033 <thida.sa@ku.th>
2021-02-15 20:11:15 +0700

    lab3_1

--- lab3_1.c
+++ lab3_1.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,max=0;
+    int i,j,max=0,imax=0;
     int s[5][4];
-    for(i=0; i<5; i++)
+    for(i=1; i<=5; i++)
     {
         int sb=0;
         for(j=0; j<4; j++)
         {
             scanf("%d",&s[i][j]);
             sb=sb+s[i][j];
-            if(sb>max)
+            if(sb>max){
                 max=sb;
+                imax=i;
+            }
         }
     }
 
-    printf("%d %d",i,max);
+    printf("%d %d",imax,max);
 
     return 0;
 }
