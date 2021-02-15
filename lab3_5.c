904673a557bc7ab9876362a1a3be2d899bb54e69
b6320503033 <thida.sa@ku.th>
2021-02-15 19:49:33 +0700

    Update lab3_5.c

--- lab3_5.c
+++ lab3_5.c
@@ -1,45 +1,44 @@
 #include<stdio.h>
 int main()
 {
-    int f=1,k,i,j;
-    scanf("%d",&k);
-    char number[k];
-    for(i = 0; i <= k; i++)
+    unsigned int n;
+    int a,i,j,f=1,k=0;
+    scanf("%d",&n);
+    if((n>0)&&(n<10000));
+    char number[n];
+
+    for(i = 0; i <= n; i++)
     {
         number[i]=1;
     }
-    for(i = 2; i <= k; i++)
-    {
+    for(i = 2; i <= n; i++)
         if(number[i]==1)
         {
-            for(j=2*i; j<=k; j=j+i)
+            for(j=2*i; j<=n; j=j+i)
             {
                 number[j]=0;
-                if(i%2==0)
-                    number[j]=0;
-                else if(i%3==0)
-                    number[j]=0;
-                else if(i%5==0)
-                    number[j]=0;
-                else if(i%7==0)
+                if(i%2==0||i%3==0||i%5==0||i%7==0)
                     number[j]=0;
             }
 
         }
-    }
-    for(i=2; i<=k; i++)
-    {
+
+    for(i=2; i<=n; i++)
+
         if(number[i]==1)
         {
-            f*=i;
-            if(f>=k)
-                break;
+            if(n%i==0)
+            {
+                f=f*i;
+                k++;
+
+            }
         }
-    }
-    if(f==k)
-        printf("%d is lucky number %d",k,f);
+
+    if(f==n&&k==3)
+        printf("%d is a Lucky Number",n);
     else
-        printf("%d is not lucky number %d",k,f);
+        printf("%d is not a Lucky Number",n);
 
     return 0;
 }
