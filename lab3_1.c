a095c73360e5ad9cbcd9e7a97d2c5cfe1b8652a5
b6320503033 <thida.sa@ku.th>
2021-02-15 17:02:14 +0700

    Initial commit

--- lab3_1.c
+++ lab3_1.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
-int main(){
+int main()
+{
+    int i,j,max=0;
+    int s[5][4];
+    for(i=0; i<5; i++)
+    {
+        int sb=0;
+        for(j=0; j<4; j++)
+        {
+            scanf("%d",&s[i][j]);
+            sb=sb+s[i][j];
+            if(sb>max)
+                max=sb;
+        }
+        printf("\n");
+    }
 
-return 0;
+    printf("%d %d",i,max);
+
+    return 0;
 }
